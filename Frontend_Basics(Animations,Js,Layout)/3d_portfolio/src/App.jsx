ea764aeac6de647033c28f490af5ebe660fa1a7e
ToniBirat7 <gotofreelancing1@gmail.com>
2025-04-28 19:50:09 +0545

    3-D Portfolio

--- Frontend_Basics(Animations,Js,Layout)/3d_portfolio/src/App.jsx
+++ Frontend_Basics(Animations,Js,Layout)/3d_portfolio/src/App.jsx
@@ -1,27 +1,43 @@
-import React from "react"
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+// Next Day Mobile Hamburger
 
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero></Hero>
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+          <div>
+            <About />
+            <Experience />
+            <Tech />
+            <Works />
+            <Feedbacks />
+            <div>
+              <Contact />
+              <StarsCanvas />
+            </div>
+          </div>
         </div>
-        <About></About>
-        <Experience></Experience>
-        <Tech></Tech>
-        <div className="relative z-0">
-          <Contact></Contact>
-          <StarsCanvas></StarsCanvas>
-        </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
 export default App;
--- Frontend_Basics(Animations,Js,Layout)/3d_portfolio/src/components/Navbar.jsx
+++ Frontend_Basics(Animations,Js,Layout)/3d_portfolio/src/components/Navbar.jsx
@@ -1,47 +1,49 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { styles } from '../styles'
-import { logo, menu, close } from '../assets'
-import { navLinks } from '../constants'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  const [active, setActive] = useState('');
+  const [active, setActive] = useState("");
+
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-      <div className='w-full flex justify-between items-center max-w-7x1 mx-auto'>
-        <Link 
-        to='/'
-        className='flex items-center gap-2'
-        onClick={() => {
-          setActive("");
-          window.scrollTo(0,0)
-        }}
+    <nav
+      className={`${styles.paddingX} flex w-full items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
         >
-          <img src={logo} alt="Logo" className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Birat | <span className='sm:block hidden'>Django Mastery</span>
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain "></img>
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Birat <span className="sm:block hidden"> | Django Mastery</span>
           </p>
         </Link>
-        <ul className='list-none hidden sm:flex gap-10'>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
-            <li 
-            key={link.id}
-            className={`${
-              active === link.title
-              ? "text-white"
-              : "text-secondary"
-            } hover:text-white text-[18px] font-medium cursor-pointer`}
-            onClick={() => {
-              setActive(link.title)
-            }}
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => {
+                setActive(link.title);
+              }}
             >
-              <a href={`#${link.id}`}>{link.title}</a>
+              <Link to={`/${link.id}`}>{link.title}</Link>
             </li>
           ))}
         </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
