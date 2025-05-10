84d130a979b7e7475c70bb2cc0ce74dbcdc18178
ToniBirat7 <gotofreelancing1@gmail.com>
2025-05-10 13:58:42 +0545

    Changes

---
+++ Frontend_Basics(Animations,Js,Layout)/Learning_CSS/my-vue-app/src/assets/Components/Day1.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Day1 = () => {
+  return <div>Hey!!!</div>;
+};
+
+export default Day1;
---
+++ Frontend_Basics(Animations,Js,Layout)/my-react-app/src/components/CSS_Learning/Day1.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Day1.css";
+
+const Day1 = () => {
+  return (
+    <div className="container">
+      <div className="box">
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Day1;
