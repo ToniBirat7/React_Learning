567343b9721073c8191553bc3b9294ffb91a086a
ToniBirat7 <gotofreelancing1@gmail.com>
2025-05-11 20:54:15 +0545

    Day3

--- Learning_NextJs/first_app/src/app/product/[productId]/page.tsx
+++ Learning_NextJs/first_app/src/app/product/[productId]/page.tsx
@@ -1,7 +1,26 @@
-export default function Product( {params}: { params: { productId: string } }) {
+import { Metadata } from "next";
+import { title } from "process";
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const { productId } = await Promise.resolve(params);
+  return {
+    title: `Product ${productId}`,
+  };
+};
+
+type Props = {
+  params: {
+    productId: String;
+  };
+};
+
+export default async function Product({ params }: Props) {
+  const { productId } = await Promise.resolve(params);
   return (
     <div>
-      <h1>Product Id {params.productId}</h1>
+      <h1>Product Id {productId}</h1>
       <p>Product details will be displayed here.</p>
     </div>
   );
