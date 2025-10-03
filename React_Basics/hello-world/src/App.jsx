adf907d4be79fff99f98a039a4b061ee9eeaf1a7
ToniBirat7 <gotofreelancing1@gmail.com>
2025-10-03 17:40:12 +0545

    docs: understanding react at its core

--- React_Basics/hello-world/src/App.jsx
+++ React_Basics/hello-world/src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = ({ props }) => {
+  return (
+    <>
+      <h2>Functional Component</h2>
+    </>
+  );
+};
 
-const App = () => {
-  
-}
-
-export default App
+export default App;
