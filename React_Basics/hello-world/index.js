0597a7be69ad26e7991ea4d2c5c26e73d08755b9
ToniBirat7 <gotofreelancing1@gmail.com>
2025-10-05 22:47:48 +0545

    docs: react cont

---
+++ React_Basics/hello-world/index.js
@@ -0,0 +1,40 @@
+const ReactX = (() => {
+  let state; // Undefined
+  const useState = (initialValue) => {
+    if (state === undefined) {
+      state = initialValue;
+    }
+
+    console.log("Current Value is :", state);
+
+    const setState = (updateValue) => {
+      state = updateValue;
+    };
+    return [state, setState];
+  };
+
+  return {
+    useState,
+  };
+})();
+
+const { useState } = ReactX;
+
+const Component = () => {
+  const [value, setValue] = useState(1);
+
+  console.log("Value : ", value);
+
+  const root = document.getElementById("root");
+
+  const btn = document.getElementById("btn");
+
+  btn.addEventListener("click", () => {
+    setValue(2);
+  });
+
+  root.innerHTML = `<h1>Current Value is ${value}`;
+};
+
+Component();
+Component(); // Re-render
