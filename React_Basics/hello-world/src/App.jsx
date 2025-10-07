60ad3ba46e3e1593408ccefefb28286a7823785b
ToniBirat7 <gotofreelancing1@gmail.com>
2025-10-07 14:13:23 +0545

    docs: suspense and tanstack query

--- React_Basics/hello-world/src/App.jsx
+++ React_Basics/hello-world/src/App.jsx
@@ -1,22 +1,24 @@
-import { useState, useEffect } from "react";
-import Card from "../src/components/Card";
-
-const App = () => {
-  const data = 0;
+import { useEffect, useState } from "react";
+function App() {
+  const [todos, setTodos] = useState([]);
   useEffect(() => {
-    data = fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-    return;
+    fetch("https://dummyjson.com/todos?limit=300")
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data.todos);
+      });
   }, []);
-
   return (
-    <>
-      <div className="">
-        <Card props={"Birat"} />
-      </div>
-    </>
+    <div>
+      <h1>To-Do List</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo} {todo.completed ? "✅" : "❌"}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-};
-
+}
 export default App;
