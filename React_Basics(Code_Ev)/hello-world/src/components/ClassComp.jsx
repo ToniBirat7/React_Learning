7208b9a3419fa915bc392015befb765fdb8ebadf
ToniBirat7 <gotofreelancing1@gmail.com>
2025-10-11 15:45:17 +0545

    docs: react cont

--- React_Basics(Code_Ev)/hello-world/src/components/ClassComp.jsx
+++ React_Basics(Code_Ev)/hello-world/src/components/ClassComp.jsx
@@ -3,5 +3,25 @@ import { Component } from "react";
 class ClassComponent extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      name: "Birat",
+    };
+
+    console.log("Lifcylce Started");
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("getDerived");
+    return null;
+  }
+
+  componentDidMount() {}
+
+  render() {
+    console.log("Render");
+    return <h1>Hiii Lifecycle</h1>;
   }
 }
+
+export default ClassComponent;
