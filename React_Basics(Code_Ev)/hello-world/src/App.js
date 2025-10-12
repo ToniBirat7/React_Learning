4c359df55b84e0a3f7eb71f851adee5fdcc59b76
ToniBirat7 <gotofreelancing1@gmail.com>
2025-10-12 12:15:03 +0545

    docs: Context API

--- React_Basics(Code_Ev)/hello-world/src/App.js
+++ React_Basics(Code_Ev)/hello-world/src/App.js
@@ -3,8 +3,10 @@ import { ErrorBoundary } from "react-error-boundary";
 import BuggyComponent from "./components/BuggyComp";
 import ErrorFallback from "./components/ErrorBoundary";
 import Parent from "./components/context/Parent";
+import { Provider, Consumer } from "./components/context/userContext";
 
 function App({ isLoggedIn, username }) {
+  console.log(`Provider : ${provider}, Consumer : ${consumer}`);
   return (
     <>
       {/* {isLoggedIn ? <h1>Hi ${username}</h1> : <Form></Form>} */}
@@ -18,6 +20,7 @@ function App({ isLoggedIn, username }) {
       >
         <BuggyComponent></BuggyComponent>
       </ErrorBoundary> */}
+      <Provider></Provider>
       <Parent name="Passed from App"></Parent>
     </>
   );
