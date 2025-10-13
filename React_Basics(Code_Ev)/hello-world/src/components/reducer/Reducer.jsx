1fa3d55b1df7cc01c9d099c627c7092982ca5564
ToniBirat7 <gotofreelancing1@gmail.com>
2025-10-13 17:26:36 +0545

    docs: useReducer

--- React_Basics(Code_Ev)/hello-world/src/components/reducer/Reducer.jsx
+++ React_Basics(Code_Ev)/hello-world/src/components/reducer/Reducer.jsx
@@ -7,6 +7,7 @@ const Reducer = () => {
   const [count, dispatch] = useReducer(reducer, initialState);
   return (
     <>
+      <h1>Count : {count}</h1>
       <button onClick={() => dispatch("increment")}></button>
       <button onClick={() => dispatch("decrement")}></button>
       <button onClick={() => dispatch("reset")}></button>
