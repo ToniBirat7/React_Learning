ce141d010b1bc514a771097e5cbfc1b5211bb471
ToniBirat7 <gotofreelancing1@gmail.com>
2025-10-13 22:58:15 +0545

    docs: useReducer

--- React_Basics(Code_Ev)/hello-world/src/App.js
+++ React_Basics(Code_Ev)/hello-world/src/App.js
@@ -1,13 +1,6 @@
-import "./App.css";
-import { ErrorBoundary } from "react-error-boundary";
-import BuggyComponent from "./components/BuggyComp";
-import ErrorFallback from "./components/ErrorBoundary";
 import Parent from "./components/context/Parent";
-// import { Provider } from "./components/context/userContext";
-import { UserContext, CounterContext } from "./components/context/userContext";
+import { CounterContext } from "./components/context/userContext";
 import { useReducer, useState } from "react";
-import Count from "./components/count/Count";
-import Reducer from "./components/reducer/Reducer";
 
 const reducer = (state, action) => {
   console.log("I am Reducer");
@@ -26,22 +19,10 @@ const reducer = (state, action) => {
   }
 };
 
-function App({ isLoggedIn, username }) {
-  const [theme, setTheme] = useState("light");
-  const [uname, setUname] = useState("Kese");
-  // const [count, setCount] = useState(0);
+function App() {
   const [state, dispatch] = useReducer(reducer, 0);
   return (
     <>
-      {/* <ErrorBoundary
-        FallbackComponent={ErrorFallback}
-        onReset={() => {
-          console.log("Reset");
-        }}
-      >
-        <BuggyComponent></BuggyComponent>
-      </ErrorBoundary> */}
-
       <CounterContext.Provider value={{ state: state, dispatch: dispatch }}>
         <Parent></Parent>
       </CounterContext.Provider>
