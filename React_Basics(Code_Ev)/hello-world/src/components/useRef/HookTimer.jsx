814d5597c6deca3e557b5a5eb58ff854c6aabc49
ToniBirat7 <gotofreelancing1@gmail.com>
2025-10-14 13:34:16 +0545

    docs: useCallback vs useMemo vs React.memo

---
+++ React_Basics(Code_Ev)/hello-world/src/components/useRef/HookTimer.jsx
@@ -0,0 +1,13 @@
+import React, { useReducer, useRef, useState } from "react";
+
+const HookTimer = () => {
+  const [timer, setTimer] = useState(0);
+  const intervalRed = useRef(null);
+  return (
+    <>
+      <div className=""></div>
+    </>
+  );
+};
+
+export default HookTimer;
