b8f1a93aa13d09c646624eb2a8f8e4eddf4d611b
ToniBirat7 <gotofreelancing1@gmail.com>
2025-10-14 13:40:25 +0545

    docs: useCallback vs useMemo vs React.memo

--- React_Basics(Code_Ev)/hello-world/src/components/useRef/HookTimer.jsx
+++ React_Basics(Code_Ev)/hello-world/src/components/useRef/HookTimer.jsx
@@ -2,23 +2,33 @@ import React, { useEffect, useReducer, useRef, useState } from "react";
 
 const HookTimer = () => {
   const [timer, setTimer] = useState(0);
+  const [open, setOpen] = useState(true);
   const intervalRef = useRef(null);
 
   useEffect(() => {
-    setInterval(() => {
+    console.log("UseEffect");
+    intervalRef.current = setInterval(() => {
       setTimer((prev) => {
         return prev + 1;
       });
     });
 
     return () => {
+      console.log("Clear");
       clearInterval(intervalRef.current);
     };
   }, []);
   return (
     <>
       <h1>Hook Timer : {timer}</h1>
-      <button>Clear Timer</button>
+      <button
+        onClick={() => {
+          setOpen(false);
+          clearInterval(intervalRef.current);
+        }}
+      >
+        Clear Timer
+      </button>
     </>
   );
 };
